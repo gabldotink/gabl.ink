dd20a09dd1c769a93da5d9cf87bcabf127cdb739
Gabriel “gabldotink” <gabl@gabl.ink>
2024-07-18 23:15:20 -0600

    implement “commit.sh” ~30%

--- bin/commit.sh
+++ bin/commit.sh
@@ -16,7 +16,11 @@ item="$1"
 readonly item
 export item
 
-# this regular expression will work later
+bin_dir="$(dirname "$script")"
+readonly bin_dir
+export bin_dir
+
+# todo: make regular expression actually work for validation
 item_regex='^[A-Za-z0-9._-]+$'
 readonly item_regex
 export item_regex
@@ -26,4 +30,42 @@ export item_regex
 
 This script updates the “date.updated” key of an item based on file
 modification times. (Or, it will, once I’m done with it.)\n' "$script"
+  exit 1
 #fi
+
+# todo: find POSIX alternative to “jq” if possible
+# todo: use parallel processes for “jq”
+year="$(jq --raw-output .date.updated.year \
+            "$bin_dir/../index/$item/info.json")"
+export year
+
+month="$(jq --raw-output .date.updated.month \
+             "$bin_dir/../index/$item/info.json")"
+export month
+
+day="$(jq --raw-output .date.updated.day \
+           "$bin_dir/../index/$item/info.json")"
+export day
+
+# todo: find POSIX alternative to “date -r”
+# todo: cache “date” outputs instead of running multiple times
+# todo: use parallel processes for “date”
+# todo: strip leading zeros
+# todo: do “test && year= ; export year” ?
+for i in "$bin_dir/../index/$item/"*;do
+  export i
+  if [ "$(date -r "$i" '+%Y')" > "$year" ];then
+    year="$(date -r "$i" '+%Y')"
+    export year
+  fi
+  if [ "$(date -r "$i" '+%m')" > "$month" ];then
+    month="$(date -r "$i" '+%m')"
+    export month
+  fi
+  if [ "$(date -r "$i" '+%d')" > "$day" ];then
+    day="$(date -r "$i" '+%d')"
+    export day
+  fi
+done
+
+# todo: have script replace values in “info.json”
