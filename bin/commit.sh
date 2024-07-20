ac5e056c51fb15971cbb08f2394a8a7dbd4c5535
Gabriel “gabldotink” <gabl@gabl.ink>
2024-07-19 21:35:19 -0600

    add hours, minutes, seconds to commit.sh
    not in any JSON yet

--- bin/commit.sh
+++ bin/commit.sh
@@ -35,37 +35,63 @@ modification times. (Or, it will, once I’m done with it.)\n' "$script"
 
 # todo: find POSIX alternative to “jq” if possible
 # todo: use parallel processes for “jq”
-year="$(jq --raw-output .date.updated.year \
-            "$bin_dir/../index/$item/info.json")"
+# “jq -r” = “jq --raw-output”
+year="$(jq -r .date.updated.year \
+              "$bin_dir/../index/$item/info.json")"
 export year
 
-month="$(jq --raw-output .date.updated.month \
-             "$bin_dir/../index/$item/info.json")"
+month="$(jq -r .date.updated.month \
+               "$bin_dir/../index/$item/info.json")"
 export month
 
-day="$(jq --raw-output .date.updated.day \
-           "$bin_dir/../index/$item/info.json")"
+day="$(jq -r .date.updated.day \
+             "$bin_dir/../index/$item/info.json")"
 export day
+
+hour="$(jq -r .date.updated.hour \
+              "$bin_dir/../index/$item/info.json")"
+export hour
+
+minute="$(jq -r .date.updated.minute \
+                "$bin_dir/../index/$item/info.json")"
+export minute
+
+second="$(jq -r .date.updated.second \
+                "$bin_dir/../index/$item/info.json")"
+export second
 
 # todo: find POSIX alternative to “date -r”
 # todo: cache “date” outputs instead of running multiple times
 # todo: use parallel processes for “date”
 # todo: strip leading zeros
-# todo: do “test && year= ; export year” ?
+# todo: do “test && year= ; export year”?
+# “date -ur” = “date --utc --reference”
 for i in "$bin_dir/../index/$item/"*;do
   export i
-  if [ "$(date -r "$i" '+%Y')" > "$year" ];then
-    year="$(date -r "$i" '+%Y')"
+  if [ "$(date -ur "$i" '+%Y')" -gt "$year" ];then
+    year="$(date -ur "$i" '+%Y')"
     export year
   fi
-  if [ "$(date -r "$i" '+%m')" > "$month" ];then
-    month="$(date -r "$i" '+%m')"
+  if [ "$(date -ur "$i" '+%m')" -gt "$month" ];then
+    month="$(date -ur "$i" '+%m')"
     export month
   fi
-  if [ "$(date -r "$i" '+%d')" > "$day" ];then
-    day="$(date -r "$i" '+%d')"
+  if [ "$(date -ur "$i" '+%d')" -gt "$day" ];then
+    day="$(date -ur "$i" '+%d')"
     export day
+  fi
+  if [ "$(date -ur "$i" '+%H')" -gt "$hour" ];then
+    hour="$(date -ur "$i" '+%d')"
+    export hour
+  fi
+  if [ "$(date -ur "$i" '+%M')" -gt "$minute" ];then
+    minute="$(date -ur "$i" '+%d')"
+    export minute
+  fi
+  if [ "$(date -ur "$i" '+%S')" -gt "$second" ];then
+    second="$(date -ur "$i" '+%S')"
+    export second
   fi
 done
 
-# todo: have script replace values in “info.json”
+# todo: replace values in “info.json”
