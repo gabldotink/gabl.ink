ccded23eda99a92ee32ac348f7c6ac47399dd1c9
Gabriel “gabldotink” <gabl@gabl.ink>
2024-07-19 22:46:22 -0600

    make “commit.sh” system time–based
    still need to add to JSON

--- bin/commit.sh
+++ bin/commit.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # SPDX-License-Identifier: CC-BY-4.0
 
-# This script updates the “date.updated” key of an item based on file
-# modification times. (Or, it will, once I’m done with it.)
+# This script updates the “date.updated” key of an item based on the
+# system time. (Or, it will, once I’m done with it.)
 
 # activate POSIX mode for Bash
 readonly POSIXLY_CORRECT
@@ -29,68 +29,23 @@ export item_regex
   printf 'usage: %s <item>
 
 This script updates the “date.updated” key of an item based on file
-modification times. (Or, it will, once I’m done with it.)\n' "$script"
+system time. (Or, it will, once I’m done with it.)\n' "$script"
   exit 1
 #fi
 
-# todo: find POSIX alternative to “jq” if possible
-# todo: use parallel processes for “jq”
-# “jq -r” = “jq --raw-output”
-year="$(jq -r .date.updated.year \
-              "$bin_dir/../index/$item/info.json")"
-export year
+# todo: find POSIX alternative to GNU date options
+epoch="$(date -u '+%-s')"
+readonly epoch
+export epoch
 
-month="$(jq -r .date.updated.month \
-               "$bin_dir/../index/$item/info.json")"
-export month
-
-day="$(jq -r .date.updated.day \
-             "$bin_dir/../index/$item/info.json")"
-export day
-
-hour="$(jq -r .date.updated.hour \
-              "$bin_dir/../index/$item/info.json")"
-export hour
-
-minute="$(jq -r .date.updated.minute \
-                "$bin_dir/../index/$item/info.json")"
-export minute
-
-second="$(jq -r .date.updated.second \
-                "$bin_dir/../index/$item/info.json")"
-export second
-
-# todo: find POSIX alternative to GNU “date” options
-# todo: cache “date” outputs instead of running multiple times
-# todo: use parallel processes for “date”
-# todo: do “test && year= ; export year”?
-# “date -ur” = “date --utc --reference”
-for i in "$bin_dir/../index/$item/"*;do
-  export i
-  if [ "$(date -ur "$i" '+%-Y')" -gt "$year" ];then
-    year="$(date -ur "$i" '+%-Y')"
-    export year
-  fi
-  if [ "$(date -ur "$i" '+%-m')" -gt "$month" ];then
-    month="$(date -ur "$i" '+%-m')"
-    export month
-  fi
-  if [ "$(date -ur "$i" '+%-d')" -gt "$day" ];then
-    day="$(date -ur "$i" '+%-d')"
-    export day
-  fi
-  if [ "$(date -ur "$i" '+%-H')" -gt "$hour" ];then
-    hour="$(date -ur "$i" '+%-H')"
-    export hour
-  fi
-  if [ "$(date -ur "$i" '+%-M')" -gt "$minute" ];then
-    minute="$(date -ur "$i" '+%-M')"
-    export minute
-  fi
-  if [ "$(date -ur "$i" '+%-S')" -gt "$second" ];then
-    second="$(date -ur "$i" '+%-S')"
-    export second
-  fi
-done
+# "date -d" = "date --date"
+year="$(date -ud "$epoch" '+%-Y')"
+month="$(date -ud "$epoch" '+%-m')"
+day="$(date -ud "$epoch" '+%-d')"
+hour="$(date -ud "$epoch" '+%-H')"
+minute="$(date -ud "$epoch" '+%-M')"
+second="$(date -ud "$epoch" '+%-S')"
+readonly year month day hour minute second
+export year month day hour minute second
 
 # todo: replace values in “info.json”
