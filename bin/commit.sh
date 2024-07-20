4fd53623ec21af5ad186706cbd33810ba66e7041
Gabriel “gabldotink” <gabl@gabl.ink>
2024-07-20 08:20:10 -0600

    finish(?) “commit.sh”
    also fixed one syntax error

--- bin/commit.sh
+++ bin/commit.sh
@@ -16,36 +16,49 @@ item="$1"
 readonly item
 export item
 
-bin_dir="$(dirname "$script")"
-readonly bin_dir
-export bin_dir
+index="$(dirname "$script")/../index"
+readonly index
+export index
 
-# todo: make regular expression actually work for validation
-item_regex='^[A-Za-z0-9._-]+$'
-readonly item_regex
-export item_regex
+# todo: implement validation
+#item_regex='^[A-Za-z0-9._-]+$'
+#readonly item_regex
+#export item_regex
 
-#if ! "$(printf '%s' "$item"|grep -E "$item_regex")";then
-  printf 'usage: %s <item>
-
-This script updates the “date.updated” key of an item based on file
-system time. (Or, it will, once I’m done with it.)\n' "$script"
-  exit 1
+#if ! "$(printf '%s\n' "$item"|grep -E "$item_regex")";then
+#  printf 'usage: %s <item>
+#
+#This script updates the “date.updated” key of an item based on the
+#system time. (Or, it will, once I’m done with it.)\n' "$script"
+#  exit 1
 #fi
 
-# todo: find POSIX alternative to GNU date options
+# todo: GNU “date” dependency
 epoch="$(date -u '+%-s')"
 readonly epoch
 export epoch
 
-# "date -d" = "date --date"
-year="$(date -ud "$epoch" '+%-Y')"
-month="$(date -ud "$epoch" '+%-m')"
-day="$(date -ud "$epoch" '+%-d')"
-hour="$(date -ud "$epoch" '+%-H')"
-minute="$(date -ud "$epoch" '+%-M')"
-second="$(date -ud "$epoch" '+%-S')"
-readonly year month day hour minute second
-export year month day hour minute second
+set_json(){
+  s="$1"
+  key="$2"
+  readonly s key
+  export s key
 
-# todo: replace values in “info.json”
+  # “date -d” = “date --date”
+  value="$(date -ud "@$epoch" "+%-$s")"
+  readonly value
+  export value
+
+  # todo: “jq” dependency
+  # todo: “sponge” dependency
+  jq --arg key "$key" --arg value "$value" \
+    '.date.updated.$key = $value' "$index/$item/info.json" \
+   |sponge "$index/$item/info.json"
+}
+
+set_json Y year
+set_json m month
+set_json d day
+set_json H hour
+set_json M minute
+set_json S second
