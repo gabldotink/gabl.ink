66ed1f77ff5f0c96a3ff41f0f7aaed2804d1bfc1
Gabriel “gabldotink” <gabl@gabl.ink>
2024-07-21 13:51:54 -0600

    reverse-recursively operate on items in “commit.sh”
    Alright, it’s time for another long commit message, but I’ll try to
    make it not as long as last time. *shudders*
    
    I’m thinking about how to internationalize stuff. Before, I was
    planning on translating the names of all the JSON keys as well as the
    content (so “title” would become “titre” in French, for example).
    Apps could determine what key names to query from an ID’s language tag.
    
    This would allow users to read the JSON in their preferred language,
    not just the generated pages, and I think that would be pretty good.
    
    However, it _would_ make it harder to write programs to work with that
    JSON. There are a few ways to make it work, though:
    
      1. Add translations in a dictionary;
      2. Give the different-language schemas the exact same key order (this
         format would be more difficult to parse);
    
    And the third, and easiest for me:
    
      3. Don’t!
    
    And just use English. But I don’t want that to be the case, so that’s
    not ideal. Oh well. Bye-bye!
    
    (P.S. Also, I’m going to start adding commit trailers.)
    
    Authored-by: Gabriel “gabldotink” <gabl@gabl.ink>
    Format: markdown
    Signed-off-by: Gabriel “gabldotink” <gabl@gabl.ink>
    SPDX-License-Identifier: CC-BY-4.0

--- bin/en/commit.sh
+++ bin/en/commit.sh
@@ -9,13 +9,14 @@ readonly POSIXLY_CORRECT
 export POSIXLY_CORRECT
 
 script="$0"
-item="$1"
+items="$1"
 # todo: improve regular expression
 item_regex='[a-z][a-z0-9_-]+$'
-readonly script item item_regex
+readonly script item_regex
 export script item item_regex
 
 if "$(printf '%s\n' "$item"|grep -Eve "$item_regex">/dev/null)";then
+  # shellcheck disable=SC1112
   printf 'usage: %s <item>
 
 This script updates the “date.updated” key of an item based on the
@@ -23,37 +24,49 @@ system time. (Or, it will, once I’m done with it.)\n' "$script"
   exit 1
 fi
 
+until [ "$(dirname "$item")" = index ];do
+  items="$item $(dirname "$item")"
+  export items
+done
+
+readonly items
+export items
+
 # todo: “readlink” dependency
 index="$(readlink --canonicalize "$(dirname "$script")/../../index")"
-info_json="$index/$item/info.json"
-# todo: GNU “date” dependency
-epoch="$(date -u '+%-s')"
-readonly index epoch
-export index epoch
+readonly index
+export index
 
-set_json(){
-  s="$1"
-  key="$2"
-  export s key
+for item in $items;do
+  export item
+  info_json="$index/$item/info.json"
+  # todo: GNU “date” dependency
+  epoch="$(date -u '+%-s')"
 
-  value="$(date -u --date="@$epoch" "+%-$s")"
-  export value
+  set_json(){
+    s="$1"
+    key="$2"
+    export s key
 
-  # todo: “jq” dependency
-  # todo: “sponge” dependency
-  jq --compact-output --arg key "$key" --argjson value "$value" \
-    '.date.updated[$key] = $value' "$info_json" \
+    value="$(date -u --date="@$epoch" "+%-$s")"
+    export value
+
+    # todo: “jq” dependency
+    # todo: “sponge” dependency
+    jq --compact-output --arg key "$key" --argjson value "$value" \
+      '.date.updated[$key] = $value' "$info_json" \
+      |sponge "$info_json">/dev/null
+  }
+
+  set_json Y year
+  set_json m month
+  set_json d day
+  set_json H hour
+  set_json M minute
+  set_json S second
+
+  jq --compact-output --arg full \
+    "$(date -u --date="@$epoch" '+%Y-%m-%dT%H:%M:%S+00:00')" \
+    '.date.updated.full = $full' "$info_json" \
     |sponge "$info_json">/dev/null
-}
-
-set_json Y year
-set_json m month
-set_json d day
-set_json H hour
-set_json M minute
-set_json S second
-
-jq --compact-output --arg full \
-  "$(date -u --date="@$epoch" '+%Y-%m-%dT%H:%M:%S+00:00')" \
-  '.date.updated.full = $full' "$info_json" \
-  |sponge "$info_json">/dev/null
+done
