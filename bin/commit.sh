5e7e23284b45870dc442ba587b17c5ae0216061e
Gabriel “gabldotink” <gabl@gabl.ink>
2024-07-21 11:38:10 -0600

    improve regular expressions
    They will get more restrictive, but probably never *less* (at least,
    once this is all stable).

--- bin/commit.sh
+++ bin/commit.sh
@@ -10,41 +10,36 @@ export POSIXLY_CORRECT
 
 script="$0"
 item="$1"
+# todo: improve regular expression
+item_regex='[a-z][a-z0-9._-]+$'
+readonly script item item_regex
+export script item item_regex
+
+if ! "$(printf '%s\n' "$item"|grep -E "$item_regex")";then
+  printf 'usage: %s <item>
+
+This script updates the “date.updated” key of an item based on the
+system time. (Or, it will, once I’m done with it.)\n' "$script"
+  exit 1
+fi
+
 index="$(dirname "$script")/../index"
-readonly script item index
-export script item index
-
-# todo: implement validation
-#item_regex='^[A-Za-z0-9._-]+$'
-#readonly item_regex
-#export item_regex
-
-#if ! "$(printf '%s\n' "$item"|grep -E "$item_regex")";then
-#  printf 'usage: %s <item>
-#
-#This script updates the “date.updated” key of an item based on the
-#system time. (Or, it will, once I’m done with it.)\n' "$script"
-#  exit 1
-#fi
-
 # todo: GNU “date” dependency
 epoch="$(date -u '+%-s')"
-readonly epoch
-export epoch
+readonly index epoch
+export index epoch
 
 set_json(){
   s="$1"
   key="$2"
   export s key
 
-  # “date -d” = “date --date”
-  value="$(date -ud "@$epoch" "+%-$s")"
+  value="$(date -u --date="@$epoch" "+%-$s")"
   export value
 
   # todo: “jq” dependency
   # todo: “sponge” dependency
-  # “jq -c” = “jq --compact-output”
-  jq -c --arg key "$key" --argjson value "$value" \
+  jq --compact-output --arg key "$key" --argjson value "$value" \
     '.date.updated[$key] = $value' "$index/$item/info.json" \
     |sponge "$index/$item/info.json"
 }
@@ -56,6 +51,6 @@ set_json H hour
 set_json M minute
 set_json S second
 
-jq -c --arg full "$(date -ud "@$epoch" '+%Y-%m-%dT%H:%M:%S+00:00')" \
+jq -c --arg full "$(date -u --date="@$epoch" '+%Y-%m-%dT%H:%M:%S+00:00')" \
   '.date.updated.full = $full' "$index/$item/info.json" \
   |sponge "$index/$item/info.json"
