c2f81ed9172c38946f0e755e047d330a510092cf
Gabriel “gabldotink” <gabl@gabl.ink>
2024-08-04 14:22:19 -0600

    add “-p” to “mkdir” in “makeroot”

--- index/en/meta/programs/makeroot/index.sh
+++ index/en/meta/programs/makeroot/index.sh
@@ -11,14 +11,14 @@ index="$(readlink --canonicalize "$(dirname "$script")/../../../..")"
 readonly root index
 export root index
 
-cp -fp "$index/en/meta/robots/index.txt" \
-       "$root/robots.txt"
-cp -fp "$index/en/meta/git/attributes/index.gitattributes" \
-       "$root/.gitattributes"
-cp -fp "$index/mul/meta/root/index.html" \
-       "$root/index.html"
-cp -fp "$index/en/meta/github/readme/index.md" \
-       "$root/readme.md"
-mkdir  "$root/.github"
-cp -fp "$index/en/meta/github/settings/index.yml" \
-       "$root/.github/settings.yml"
+cp -fp   "$index/en/meta/robots/index.txt" \
+         "$root/robots.txt"
+cp -fp   "$index/en/meta/git/attributes/index.gitattributes" \
+         "$root/.gitattributes"
+cp -fp   "$index/mul/meta/root/index.html" \
+         "$root/index.html"
+cp -fp   "$index/en/meta/github/readme/index.md" \
+         "$root/readme.md"
+mkdir -p "$root/.github"
+cp -fp   "$index/en/meta/github/settings/index.yml" \
+         "$root/.github/settings.yml"
