fe14a95025f41f6f1b15359cee1ee8c5c78d81fd
Gabriel “gabldotink” <gabl@gabl.ink>
2025-10-27 22:52:54 -0600

    shorten function names in make_nav_buttons.sh

--- cms/scripts/lib/make_nav_buttons.sh
+++ cms/scripts/lib/make_nav_buttons.sh
@@ -7,76 +7,76 @@
 # TODO: Handle multiple chapters.
 # TODO: Handle quotation marks in other page titles.
 
-make_nav_buttons_first_last() {
-  make_nav_buttons_first_last_d="$1"
+make_nav_buttons_f_l() {
+  make_nav_buttons_f_l_d="$1"
 
-  if   [ "${make_nav_buttons_first_last_d}" = '<' ];then
-    make_nav_buttons_first_last_a=⇦
-    make_nav_buttons_first_last_i=first
-    make_nav_buttons_first_last_l=first
-    make_nav_buttons_first_last_u=First
-    make_nav_buttons_first_last_z=prev
-  elif [ "${make_nav_buttons_first_last_d}" = '>' ];then
-    make_nav_buttons_first_last_a=⇨
-    make_nav_buttons_first_last_i=last
-    make_nav_buttons_first_last_l=last
-    make_nav_buttons_first_last_u=Last
-    make_nav_buttons_first_last_z=next
+  if   [ "${make_nav_buttons_f_l_d}" = '<' ];then
+    make_nav_buttons_f_l_a=⇦
+    make_nav_buttons_f_l_i=first
+    make_nav_buttons_f_l_l=first
+    make_nav_buttons_f_l_u=First
+    make_nav_buttons_f_l_z=prev
+  elif [ "${make_nav_buttons_f_l_d}" = '>' ];then
+    make_nav_buttons_f_l_a=⇨
+    make_nav_buttons_f_l_i=last
+    make_nav_buttons_f_l_l=last
+    make_nav_buttons_f_l_u=Last
+    make_nav_buttons_f_l_z=next
   else
-    error 'make_nav_buttons_first_last direction is not f or l'
+    error 'make_nav_buttons_f_l direction is not f or l'
   fi
 
-  printf '<div class="nav_button" id="nav_%s_buttons_%s" ' "${make_nav_buttons_l}" "${make_nav_buttons_first_last_i}"
+  printf '<div class="nav_button" id="nav_%s_buttons_%s" ' "${make_nav_buttons_l}" "${make_nav_buttons_f_l_i}"
 
-  if [ "$(eval 'printf "%s" "${'"${make_nav_buttons_first_last_z}"'}"')" = null ];then
-    printf 'title="%s in %s (This is the %s page!)">' "${make_nav_buttons_first_last_u}" "${container}" "${make_nav_buttons_first_last_l}"
+  if [ "$(eval 'printf "%s" "${'"${make_nav_buttons_f_l_z}"'}"')" = null ];then
+    printf 'title="%s in %s (This is the %s page!)">' "${make_nav_buttons_f_l_u}" "${container}" "${make_nav_buttons_f_l_l}"
   else
-    printf 'title="%s in %s (“%s”)">' "${make_nav_buttons_first_last_u}" "${container}" "$(eval 'printf "%s" "${container_'"${make_nav_buttons_first_last_i}"'_title_nested_text}"')"
-    printf '<a href="../%s/" hreflang="en-US" type="text/html">' "$(zero_pad 2 "$(eval 'printf "%s" "${container_'"${make_nav_buttons_first_last_i}"'}"')")"
+    printf 'title="%s in %s (“%s”)">' "${make_nav_buttons_f_l_u}" "${container}" "$(eval 'printf "%s" "${container_'"${make_nav_buttons_f_l_i}"'_title_nested_text}"')"
+    printf '<a href="../%s/" hreflang="en-US" type="text/html">' "$(zero_pad 2 "$(eval 'printf "%s" "${container_'"${make_nav_buttons_f_l_i}"'}"')")"
   fi
 
-  printf '<p><span class="nav_button_arrow" data-ssml-sub-alias=" ">%s</span><br/>%s</p>' "${make_nav_buttons_first_last_a}" "${make_nav_buttons_first_last_u}"
+  printf '<p><span class="nav_button_arrow" data-ssml-sub-alias=" ">%s</span><br/>%s</p>' "${make_nav_buttons_f_l_a}" "${make_nav_buttons_f_l_u}"
 
-  [ "$(eval 'printf "%s" "${container_'"${make_nav_buttons_first_last_i}"'}"')" != null ] &&
+  [ "$(eval 'printf "%s" "${container_'"${make_nav_buttons_f_l_i}"'}"')" != null ] &&
     printf '</a>'
   
   printf '</div>'
 }
 
-make_nav_buttons_prev_next() {
-  make_nav_buttons_prev_next_d="$1"
+make_nav_buttons_p_n() {
+  make_nav_buttons_p_n_d="$1"
 
-  if   [ "${make_nav_buttons_prev_next_d}" = '<' ];then
-    make_nav_buttons_prev_next_a=←
-    make_nav_buttons_prev_next_i=prev
+  if   [ "${make_nav_buttons_p_n_d}" = '<' ];then
+    make_nav_buttons_p_n_a=←
+    make_nav_buttons_p_n_i=prev
     # We don’t use this variable, but I’m keeping it in case other languages need it.
     # shellcheck disable=SC2034
-    make_nav_buttons_prev_next_l=previous
-    make_nav_buttons_prev_next_u=Previous
-    make_nav_buttons_prev_next_z=first
-  elif [ "${make_nav_buttons_prev_next_d}" = '>' ];then
-    make_nav_buttons_prev_next_a=→
-    make_nav_buttons_prev_next_i=next
+    make_nav_buttons_p_n_l=previous
+    make_nav_buttons_p_n_u=Previous
+    make_nav_buttons_p_n_z=first
+  elif [ "${make_nav_buttons_p_n_d}" = '>' ];then
+    make_nav_buttons_p_n_a=→
+    make_nav_buttons_p_n_i=next
     # shellcheck disable=SC2034
-    make_nav_buttons_prev_next_l=next
-    make_nav_buttons_prev_next_u=Next
-    make_nav_buttons_prev_next_z=last
+    make_nav_buttons_p_n_l=next
+    make_nav_buttons_p_n_u=Next
+    make_nav_buttons_p_n_z=last
   else
-    error 'make_nav_buttons_prev_next direction is not p or n'
+    error 'make_nav_buttons_p_n direction is not p or n'
   fi
 
-  printf '<div class="nav_button" id="nav_%s_buttons_%s" ' "${make_nav_buttons_l}" "${make_nav_buttons_prev_next_i}"
+  printf '<div class="nav_button" id="nav_%s_buttons_%s" ' "${make_nav_buttons_l}" "${make_nav_buttons_p_n_i}"
 
-  if [ "$(eval 'printf "%s" "${'"${make_nav_buttons_prev_next_i}"'}"')" = null ];then
-    printf 'title="%s (This is the %s page!)">' "${make_nav_buttons_prev_next_u}" "${make_nav_buttons_prev_next_z}"
+  if [ "$(eval 'printf "%s" "${'"${make_nav_buttons_p_n_i}"'}"')" = null ];then
+    printf 'title="%s (This is the %s page!)">' "${make_nav_buttons_p_n_u}" "${make_nav_buttons_p_n_z}"
   else
-    printf 'title="%s (“%s”)">' "${make_nav_buttons_prev_next_u}" "$(eval 'printf "%s" "${'"${make_nav_buttons_prev_next_i}"'_title_nested_text}"')"
-    printf '<a href="../%s/" rel="%s" hreflang="en-US" type="text/html">' "$(zero_pad 2 "$(eval 'printf "%s" "${'"${make_nav_buttons_prev_next_i}"'}"')")" "${make_nav_buttons_prev_next_i}"
+    printf 'title="%s (“%s”)">' "${make_nav_buttons_p_n_u}" "$(eval 'printf "%s" "${'"${make_nav_buttons_p_n_i}"'_title_nested_text}"')"
+    printf '<a href="../%s/" rel="%s" hreflang="en-US" type="text/html">' "$(zero_pad 2 "$(eval 'printf "%s" "${'"${make_nav_buttons_p_n_i}"'}"')")" "${make_nav_buttons_p_n_i}"
   fi
 
-  printf '<p><span class="nav_button_arrow" data-ssml-sub-alias=" ">%s</span><br/>%s</p>' "${make_nav_buttons_prev_next_a}" "${make_nav_buttons_prev_next_u}"
+  printf '<p><span class="nav_button_arrow" data-ssml-sub-alias=" ">%s</span><br/>%s</p>' "${make_nav_buttons_p_n_a}" "${make_nav_buttons_p_n_u}"
 
-  [ "$(eval 'printf "%s" "${'"${make_nav_buttons_prev_next_i}"'}"')" != null ] &&
+  [ "$(eval 'printf "%s" "${'"${make_nav_buttons_p_n_i}"'}"')" != null ] &&
     printf '</a>'
   
   printf '</div>'
@@ -87,13 +87,13 @@ make_nav_buttons() {
   
   printf '<div id="nav_%s_buttons">' "${make_nav_buttons_l}"
   
-  make_nav_buttons_first_last '<'
+  make_nav_buttons_f_l '<'
   
-  make_nav_buttons_prev_next '<'
+  make_nav_buttons_p_n '<'
   
-  make_nav_buttons_prev_next '>'
+  make_nav_buttons_p_n '>'
 
-  make_nav_buttons_first_last '>'
+  make_nav_buttons_f_l '>'
 
   printf '</div>'
 }
