51b9375b7ec280e9463f60055ad97123e046df2b
Gabriel “gabldotink” <gabl@gabl.ink>
2025-10-29 14:09:05 -0600

    remove extra whitespace

--- cms/scripts/build.sh
+++ cms/scripts/build.sh
@@ -83,12 +83,12 @@ items="${index}/en/jrco_beta/01/data.json"
 
 for i in ${items};do
   type="$(jqr .type "${i}")"
-  
+
   [ "${type}" = comic_series ] &&
     continue
 
   lang="$(jqr .language "${i}")"
-  
+
   copyright_license="$(jqr .copyright.license[0] "${i}")"
   # Literal quotation marks should be used when inserting variables into jq (hyphens can cause issues).
   # shellcheck disable=SC2016
@@ -115,7 +115,7 @@ for i in ${items};do
     title_nested_text="${title_text}"
     title_nested_html="${title_html}"
   fi
-  
+
   # This is dumb
   #if [ "${lang}" = en ];then
   #  if printf '%s' "${title_text}" | grep -Fqe '“' -e '”';then
@@ -124,7 +124,7 @@ for i in ${items};do
   #      printf '%s' "${title_text}" | sed -e 's/“/'"‘/g" -e 's/”/'"’/g" -e "s/‘"'/“/g' \
   #                                        -e "s/’"'$/”/' -e "s/’"'\([^[:alnum:]_[:space:]]\)/”\1/g'
   #    else
-  #      
+  #
   #    fi
   #  fi
   #fi
@@ -139,22 +139,22 @@ for i in ${items};do
 
   {
     printf '<!DOCTYPE html>'
-  
+
     printf '<html lang="%s" dir="%s" xmlns="http://www.w3.org/1999/xhtml" xml:lang="%s">\n' \
            "${lang_bcp_47_full}" "${lang_dir}" "${lang_bcp_47_full}"
-  
+
     printf '<!-- SPDX-License-Identifier: %s -->\n' "${copyright_license_spdx}"
-  
+
     printf '<head>'
     printf '<meta charset="utf-8"/>'
     printf '<meta name="viewport" content="width=device-width,initial-scale=1"/>'
-  
+
     printf '<title>gabl.ink – %s</title>' "${title_text}"
-  
+
     printf '<meta name="description" content="%s"/>' "${description_text}"
     printf '<meta name="robots" content="index,follow"/>'
     printf '<link rel="canonical" href="%s" hreflang="%s" type="text/html"/>' "${canonical}" "${lang_bcp_47_full}"
-  
+
     if [ "${type}" = comic_page ];then
       first_published_d="$(jqr .first_published.d "${i}")"
       zero_pad_2_first_published_d="$(zero_pad 2 "${first_published_d}")"
@@ -172,7 +172,7 @@ for i in ${items};do
       series_title_html="$(jqr .title.html "${index}/${id}/../data.json")"
       series_title_text="$(jqr .title.text "${index}/${id}/../data.json")"
       volume="$(jqr .location.volume "${i}")"
-  
+
       # For future reference: Each video should have a WebM (VP9/Opus) and MP4 (H.264/AAC) version.
       # WebM should be preferred due to being free (libre), and MP4 should be provided as a fallback for compatibility.
       # In case of a video, image.png should act as a thumbnail.
@@ -181,16 +181,16 @@ for i in ${items};do
       else
         unset video_exists
       fi
-  
+
       # Determine how many directories deep from the series the page is
       up_directories=4
-  
+
       [ "${volume}" = null ] &&
         up_directories="$((up_directories-1))"
-  
+
       [ "${chapter}" = null ] &&
         up_directories="$((up_directories-1))"
-  
+
       styles="$(
         # ShellCheck warns “n” is unused, but that’s intentional
         # shellcheck disable=SC2034
@@ -199,7 +199,7 @@ for i in ${items};do
         done
         printf ../../cms/styles
       )"
-  
+
       if   [ "${up_directories}" -eq 2 ];then
         container=series
       elif [ "${up_directories}" -eq 3 ];then
@@ -209,7 +209,7 @@ for i in ${items};do
       else
         error 'up_directories is not 2, 3, or 4'
       fi
-  
+
       printf '<link rel="preload" href="%s/global.css" as="style" hreflang="zxx" type="text/css"/>' \
              "${styles}"
       printf '<link rel="preload" href="%s/comic_page_%s.css" as="style" hreflang="zxx" type="text/css"/>' \
@@ -218,9 +218,9 @@ for i in ${items};do
              "${styles}"
       printf '<link rel="stylesheet" href="%s/comic_page_%s.css" hreflang="zxx" type="text/css"/>' \
              "${styles}" "${series}"
-  
+
       printf '<link rel="license" href="%s" hreflang="en" type="text/html"/>' "${copyright_license_url}"
-  
+
       if   [ "${up_directories}" -eq 2 ];then
         unset volume chapter
       elif [ "${up_directories}" -eq 3 ];then
@@ -241,7 +241,7 @@ for i in ${items};do
       [ "${next}" != null ] &&
         zero_pad_2_next="$(zero_pad 2 "${next}")"
       zero_pad_2_page="$(zero_pad 2 "${page}")"
-  
+
       if   [ "${prev}" = null ];then
         # This is the first page, so no prefetches are needed.
         true
@@ -255,7 +255,7 @@ for i in ${items};do
         printf '<link rel="prev prefetch" href="../%s/" hreflang="%s" type="text/html"/>' \
                "${zero_pad_2_prev}" "${lang_bcp_47_full}"
       fi
-  
+
       if   [ "${next}" = null ];then
         # This is the last page, so no prefetches are needed.
         true
@@ -269,7 +269,7 @@ for i in ${items};do
         printf '<link rel="next prefetch" href="../%s/" hreflang="%s" type="text/html"/>' \
                "${zero_pad_2_next}" "${lang_bcp_47_full}"
       fi
-  
+
       make_og type article
       make_og title "${title_text}"
       make_og description "${description_text}"
@@ -281,9 +281,9 @@ for i in ${items};do
         make_og video "${canonical}video.mp4"
       fi
       make_og locale "${lang_ogp_full}"
-  
+
       printf '</head>'
-  
+
       printf '<body>'
       printf '<header>'
       printf '<a href="https://gabl.ink/">'
@@ -294,7 +294,7 @@ for i in ${items};do
       printf '<div id="nav_top">'
       printf '<h1 id="nav_top_title">'
       printf '“<cite>%s</cite>”</h1>' "${title_nested_html}"
-  
+
       if [ "${container_first}" != null ];then
         container_first_title_text="$(jqr .title.text "${index}/${id}/../${zero_pad_2_container_first}/data.json")"
         container_first_title_nested_text="$(jqr .title.nested.text "${index}/${id}/../${zero_pad_2_container_first}/data.json")"
@@ -303,7 +303,7 @@ for i in ${items};do
       else
         unset container_first_title_text container_first_title_nested_text
       fi
-  
+
       if [ "${prev}" != null ];then
         prev_title_text="$(jqr .title.text "${index}/${id}/../${zero_pad_2_prev})/data.json")"
         prev_title_nested_text="$(jqr .title.nested.text "${index}/${id}/../${zero_pad_2_prev}/data.json")"
@@ -312,7 +312,7 @@ for i in ${items};do
       else
         unset prev_title_text prev_title_nested_text
       fi
-  
+
       if [ "${next}" != null ];then
         next_title_text="$(jqr .title.text "${index}/${id}/../${zero_pad_2_next}/data.json")"
         next_title_nested_text="$(jqr .title.nested.text "${index}/${id}/../${zero_pad_2_next}/data.json")"
@@ -321,7 +321,7 @@ for i in ${items};do
       else
         unset next_title_text next_title_nested_text
       fi
-  
+
       if [ "${container_last}" != null ];then
         container_last_title_text="$(jqr .title.text "${index}/${id}/../${zero_pad_2_container_last}/data.json")"
         container_last_title_nested_text="$(jqr .title.nested.text "${index}/${id}/../${zero_pad_2_container_last}/data.json")"
@@ -330,13 +330,13 @@ for i in ${items};do
       else
         unset container_last_title_text container_last_title_nested_text
       fi
-  
+
       make_nav_buttons top
-  
+
       printf '</div>'
-  
+
       printf '<div id="comic_page_'
-  
+
       # TODO: Edge case: one format exists, other doesn’t
       # TODO: Edge case: no captions
       # TODO: Browsers fix “id” for filesystems, but we should get better filenames, in addition to easy download links
@@ -356,34 +356,34 @@ for i in ${items};do
         printf '<img src="./image.png" alt="See “Comic transcript” below"/>'
         printf '</picture></div>'
       fi
-  
+
       printf '<div id="nav_bottom">'
-  
+
       make_nav_buttons bottom
-  
+
       printf '<div id="nav_bottom_list">'
-  
+
       printf '<details id="nav_bottom_list_root">'
-  
+
       printf '<summary>'
-  
+
       printf '<i><cite>'
-  
+
       # TODO: Support higher containers (volumes and chapters).
-  
+
       if [ "${container}" = series ];then
         printf '%s' "${series_title_html}"
         printf '</cite></i>'
       fi
-  
+
       printf ', page %s ' "${page}"
-  
+
       printf '“<cite>%s</cite>”' "${title_nested_html}"
-  
+
       printf '</summary>'
-  
+
       printf '<ol id="nav_bottom_list_pages">'
-  
+
       find "${index}/${id}/.." -type f -path "${index}/${id}"'/../*/data.json' -exec sh -c '
         d="$1"
         p="$2"
@@ -396,7 +396,7 @@ for i in ${items};do
         fi
 
         printf "<li>“"
-  
+
         if [ "${s}" = "${p}" ];then
           printf "<b><cite>"
           printf "%s" "${title_nested_html}"
@@ -409,20 +409,20 @@ for i in ${items};do
           printf "</cite></a>”</li>"
         fi
       ' shell '{}' "${zero_pad_2_page}" ';'
-  
+
       printf '</ol></details></div></div>'
-  
+
       printf '<details id="comic_transcript" open="">'
-  
+
       printf '<summary>Comic transcript</summary>'
-  
+
       printf '<table id="comic_transcript_table">'
-  
+
       printf '<thead><tr>'
       printf '<th scope="col">Speaker</th>'
       printf '<th scope="col">Text</th>'
       printf '</tr></thead>'
-  
+
       for l in $(jqr '.transcript.lines|to_entries|.[].key' "${i}");do
         h="$(jqr ".transcript.lines[${l}].h" "${i}")"
         d="$(jqr ".transcript.lines[${l}].d" "${i}")"
@@ -434,17 +434,17 @@ for i in ${items};do
         printf '<td><p>%s</p></td>' "${d}"
         printf '</tr>'
       done
-  
+
       printf '</table></details>'
-  
+
       printf '<p id="first_published">First published <time class="nw" datetime="'
-  
+
       printf '%s-%s-%s' "${zero_pad_4_first_published_y}" \
                         "${zero_pad_2_first_published_m}" \
                         "${zero_pad_2_first_published_d}"
-  
+
       printf '">'
-  
+
       printf '%s ' "$(jqr ".months.\"${lang}\"[$((first_published_m-1))]" "${dict}/month_gregorian.json")"
       printf '<span data-ssml-say-as="date" data-ssml-say-as-format="d">%s</span>, ' "${first_published_d}"
       if   [ "${#first_published_y}" -lt 4 ] &&
@@ -463,9 +463,9 @@ for i in ${items};do
       else
         printf '<span data-ssml-say-as="date" data-ssml-say-as-format="y">%s</span>' "${first_published_y}"
       fi
-  
+
       printf '</time></p><article id="post_'
-  
+
       for p in $(jqr '.post|to_entries|.[].key' "${i}");do
         post_content="$(jqr ".post[${p}].content.html" "${i}")"
         post_date_d="$(jqr ".post[${p}].date.d" "${i}")"
@@ -474,16 +474,16 @@ for i in ${items};do
         zero_pad_2_post_date_m="$(zero_pad 2 "${post_date_m}")"
         post_date_y="$(jqr ".post[${p}].date.y" "${i}")"
         zero_pad_4_post_date_y="$(zero_pad 4 "${post_date_y}")"
-  
+
         printf '%s-%s-%s">' "${zero_pad_4_post_date_y}" \
                             "${zero_pad_2_post_date_m}" \
                             "${zero_pad_2_post_date_d}"
-  
+
         printf '<h2 class="nw">'
         printf '<time datetime="%s-%s-%s">' "${zero_pad_4_post_date_y}" \
                                             "${zero_pad_2_post_date_m}" \
                                             "${zero_pad_2_post_date_d}"
-  
+
         printf '%s ' "$(jqr ".months.\"${lang}\"[$((post_date_m-1))]" "${dict}/month_gregorian.json")"
         printf '<span data-ssml-say-as="date" data-ssml-say-as-format="d">%s</span>, ' "${post_date_d}"
         if   [ "${#post_date_y}" -lt 4 ] &&
@@ -502,42 +502,42 @@ for i in ${items};do
         else
           printf '<span data-ssml-say-as="date" data-ssml-say-as-format="y">%s</span>' "${first_published_y}"
         fi
-  
+
         printf '</time></h2>'
-  
+
         printf '%s' "${post_content}"
-  
+
         printf '</article>'
       done
-  
+
       printf '</main>'
-  
+
       printf '<details id="share_links">'
       printf '<summary>Share this page</summary>'
       printf '<ul>'
-  
+
       make_share_link x "${x_or_twitter}" https://x.com/intent/tweet text url hashtags \
                      "$(
                         printf 'gabl.ink @gabldotink: “%s”: “' "${series_title_text}"
                         printf '%s”' "${title_nested_text}"
                       )" \
                      "gabldotink,${series_hashtag}"
-      
+
       make_share_link reddit Reddit 'https://www.reddit.com/submit?type=LINK' title url '' \
                      "$(
                         printf 'gabl.ink: “%s”: “' "${series_title_text}"
                         printf '%s”' "${title_nested_text}"
                       )"
-      
+
       make_share_link facebook Facebook https://www.facebook.com/sharer/sharer.php '' u '' ''
-      
+
       make_share_link telegram Telegram https://t.me/share text url '' \
                      "$(
                         printf 'gabl.ink: “%s”: “' "${series_title_text}"
                         printf '%s” ' "${title_nested_text}"
                         printf '#gabldotink #%s' "${series_hashtag}"
                       )"
-      
+
       make_share_link bluesky Bluesky https://bsky.app/intent/compose text '' '' \
                      "$(
                         printf 'gabl.ink @gabl.ink: “%s”: “' "${series_title_text}"
@@ -545,42 +545,42 @@ for i in ${items};do
                         printf '%s ' "${canonical}"
                         printf '#gabldotink #%s' "${series_hashtag}"
                       )"
-      
+
       make_share_link whatsapp WhatsApp https://wa.me/ text '' '' \
                      "$(
                         printf 'gabl.ink: “%s”: “' "${series_title_text}"
                         printf '%s” ' "${title_nested_text}"
                         printf '%s' "${canonical}"
                       )"
-      
+
       make_share_link mastodon Mastodon https://mastodonshare.com/ text url '' \
                      "$(
                         printf 'gabl.ink @gabldotink@mstdn.party: “%s”: “' "${series_title_text}"
                         printf '%s” ' "${title_nested_text}"
                         printf '#gabldotink #%s' "${series_hashtag}"
                       )"
-      
+
       make_share_link threads Threads https://www.threads.com/intent/post text url '' \
                      "$(
                         printf 'gabl.ink: “%s”: “' "${series_title_text}"
                         printf '%s” ' "${title_nested_text}"
                         printf '#gabldotink #%s' "${series_hashtag}"
                       )"
-  
+
       make_share_link truthsocial 'Truth Social' https://truthsocial.com/share text url '' \
                      "$(
                         printf 'gabl.ink: “%s”: “' "${series_title_text}"
                         printf '%s” ' "${title_nested_text}"
                         printf '#gabldotink #%s' "${series_hashtag}"
                       )"
-  
+
       make_share_link gab Gab https://gab.com/compose text url '' \
                      "$(
                         printf 'gabl.ink: “%s”: “' "${series_title_text}"
                         printf '%s” ' "${title_nested_text}"
                         printf '#gabldotink #%s' "${series_hashtag}"
                       )"
-  
+
       printf '</ul></details>'
 
       printf '<details id="validate_links">'
@@ -593,7 +593,7 @@ for i in ${items};do
     fi
 
     printf '</ul></details>'
-  
+
     printf '<footer><p><span class="nw">'
     printf '<abbr title="Copyright">©</abbr> '
     printf '<time data-ssml-say-as="date" data-ssml-say-as-format="y">%s</time>' "${copyright_year_first}"
@@ -616,7 +616,7 @@ for i in ${items};do
     fi
 
     printf '</footer>'
-  
+
     printf '</body></html>\n'
   } > "${index}/${id}/index.html"
 done
