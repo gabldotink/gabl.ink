fb8ada77b5519fd8918b0861050fee126cfb73c0
Gabriel “gabldotink” <gabl@gabl.ink>
2026-02-07 00:46:05 -0700

    there are so many XMP properties D:

--- dump/reset.sh
+++ dump/reset.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm -f image_with_metadata.png image_with_metadata.png_original
 cp image.png image_with_metadata.png
-exiftool -@ ./exiftool_meta.txt ./image_with_metadata.png
-exiftool -a -G ./image_with_metadata.png
+exiftool -api LargeFileSupport=1 -api ImageHashType=SHA256 -@ ./exiftool_meta.txt ./image_with_metadata.png
+exiftool -G -a ./image_with_metadata.png
