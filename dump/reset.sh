4be4a7f30740d8b919f4317467960227d419cc85
Gabriel “gabldotink” <gabl@gabl.ink>
2026-02-08 23:40:41 -0700

    when I’m done, I’m gonna make a video showing off all the cool stuff this does that nobody cares about

--- dump/reset.sh
+++ dump/reset.sh
@@ -2,4 +2,4 @@
 rm -f image_with_metadata.png image_with_metadata.png_original
 cp image.png image_with_metadata.png
 exiftool -api LargeFileSupport=1 -api ImageHashType=SHA256 -@ ./exiftool_meta.txt ./image_with_metadata.png
-exiftool -G -a ./image_with_metadata.png
+exiftool -G -a -n ./image_with_metadata.png
