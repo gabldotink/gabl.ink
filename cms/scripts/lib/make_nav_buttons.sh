8f30d325118f5a7e7083ef0c1578a5e9183a6eb0
Gabriel “gabldotink” <gabl@gabl.ink>
2026-02-25 01:35:00 -0700

    internationalize nav buttons, mostly
    there’s still some hardcoded English but I will clean up LATER

--- cms/scripts/lib/make_nav_buttons.sh
+++ cms/scripts/lib/make_nav_buttons.sh
@@ -13,13 +13,13 @@ make_nav_buttons_f_l(){
     make_nav_buttons_f_l_a=⇦
     make_nav_buttons_f_l_i=first
     make_nav_buttons_f_l_l=first
-    make_nav_buttons_f_l_u=First
+    make_nav_buttons_f_l_u="$(printf_l10n nav_button_first)"
     make_nav_buttons_f_l_z=prev
   elif [ "${make_nav_buttons_f_l_d}" = '>' ];then
     make_nav_buttons_f_l_a=⇨
     make_nav_buttons_f_l_i=last
     make_nav_buttons_f_l_l=last
-    make_nav_buttons_f_l_u=Last
+    make_nav_buttons_f_l_u="$(printf_l10n nav_button_last)"
     make_nav_buttons_f_l_z=next
   else
     error 'make_nav_buttons_f_l direction is not < or >'
@@ -28,9 +28,9 @@ make_nav_buttons_f_l(){
   printf '<div class="nav_button" id="nav_%s_buttons_%s" ' "${make_nav_buttons_l}" "${make_nav_buttons_f_l_i}"
 
   if [ "$(eval 'printf "%s" "${'"${make_nav_buttons_f_l_z}"'}"')" = null ];then
-    printf 'title="%s in %s (This is the %s page!)">' "${make_nav_buttons_f_l_u}" "${container}" "${make_nav_buttons_f_l_l}"
+    printf 'title="%s">' "$(printf_l10n this_is_x_page "$(printf_l10n "nav_button_${make_nav_buttons_f_l_l}_inline")")"
   else
-    printf 'title="%s in %s (“%s”)">' "${make_nav_buttons_f_l_u}" "${container}" "$(eval 'printf "%s" "${container_'"${make_nav_buttons_f_l_i}"'_title_text}"')"
+    printf 'title="%s">' "$(printf_l10n nav_button_page_title "$(eval 'printf "%s" "${container_'"${make_nav_buttons_f_l_i}"'_title_text}"')")"
     printf '<a href="../../%s/%s/" hreflang="%s" type="text/html">' "$(printf '%02d' "$(eval 'printf "%s" "${container_'"${make_nav_buttons_f_l_i}"'}"')")" "${lang}" "${lang}"
   fi
 
@@ -51,14 +51,14 @@ make_nav_buttons_p_n(){
     # We don’t use this variable, but I’m keeping it in case other languages need it.
     # shellcheck disable=2034
     make_nav_buttons_p_n_l=previous
-    make_nav_buttons_p_n_u=Previous
+    make_nav_buttons_p_n_u="$(printf_l10n nav_button_prev)"
     make_nav_buttons_p_n_z=first
   elif [ "${make_nav_buttons_p_n_d}" = '>' ];then
     make_nav_buttons_p_n_a=→
     make_nav_buttons_p_n_i=next
     # shellcheck disable=2034
     make_nav_buttons_p_n_l=next
-    make_nav_buttons_p_n_u=Next
+    make_nav_buttons_p_n_u="$(printf_l10n nav_button_next)"
     make_nav_buttons_p_n_z=last
   else
     error 'make_nav_buttons_p_n direction is not < or >'
@@ -67,9 +67,9 @@ make_nav_buttons_p_n(){
   printf '<div class="nav_button" id="nav_%s_buttons_%s" ' "${make_nav_buttons_l}" "${make_nav_buttons_p_n_i}"
 
   if [ "$(eval 'printf "%s" "${'"${make_nav_buttons_p_n_i}"'}"')" = null ];then
-    printf 'title="%s (This is the %s page!)">' "${make_nav_buttons_p_n_u}" "${make_nav_buttons_p_n_z}"
+    printf 'title="%s">' "$(printf_l10n this_is_x_page "$(printf_l10n "nav_button_${make_nav_buttons_p_n_z}_inline")")"
   else
-    printf 'title="%s (“%s”)">' "${make_nav_buttons_p_n_u}" "$(eval 'printf "%s" "${'"${make_nav_buttons_p_n_i}"'_title_text}"')"
+    printf 'title="%s">' "$(printf_l10n nav_button_page_title "$(eval 'printf "%s" "${'"${make_nav_buttons_p_n_i}"'_title_text}"')")"
     printf '<a href="../../%s/%s/" rel="%s" hreflang="%s" type="text/html">' "$(printf '%02d' "$(eval 'printf "%s" "${'"${make_nav_buttons_p_n_i}"'}"')")" "${lang}" "${make_nav_buttons_p_n_i}" "${lang}"
   fi
 
