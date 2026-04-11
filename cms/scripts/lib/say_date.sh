5cf1b16284fb081ce718beada3cfdcb498aa0324
Gabriel “gabldotink” <gabl@gabl.ink>
2026-04-11 00:26:43 -0600

    fully support negative dates, because I am so bored!

--- cms/scripts/lib/say_date.sh
+++ cms/scripts/lib/say_date.sh
@@ -20,11 +20,11 @@ say_date(){
   if [ "$lang_l" = en ];then
     printf '%s\302\240' "$say_date_m_html"
     printf '<span data-ssml-say-as="date" data-ssml-say-as-format="d">%s</span>, ' "$say_date_d"
-    if   [ "${#say_date_y}" -lt 4 ] &&
-         [ "$say_date_y" -ne 0 ];then
+    if   [ "$say_date_y" -lt 1000 ] &&
+         [ "$say_date_y" -gt 0 ];then
       printf '<abbr title="anno Domini">AD</abbr>\302\240<span data-ssml-say-as="date" data-ssml-say-as-format="y">%s</span>' "$say_date_y"
-    elif [ "$say_date_y" -eq 0 ];then
-      printf '<span data-ssml-say-as="date" data-ssml-say-as-format="y">1</span>\302\240<abbr title="before Christ">BC</abbr>'
+    elif [ "$say_date_y" -le 0 ];then
+      printf '<span data-ssml-say-as="date" data-ssml-say-as-format="y">%s</span>\302\240<abbr title="before Christ">BC</abbr>' "$((1+$((-say_date_y))))"
     else
       printf '<span data-ssml-say-as="date" data-ssml-say-as-format="y">%s</span>' "$say_date_y"
     fi
@@ -35,22 +35,22 @@ say_date(){
       printf '<span data-ssml-say-as="date" data-ssml-say-as-format="d">%s</span>' "$say_date_d"
     fi
     printf '\302\240%s ' "$say_date_m_html"
-    if   [ "${#say_date_y}" -lt 4 ] &&
-         [ "$say_date_y" -ne 0 ];then
+    if   [ "$say_date_y" -lt 1000 ] &&
+         [ "$say_date_y" -gt 0 ];then
       printf '<span data-ssml-say-as="date" data-ssml-say-as-format="y">%s</span>\302\240<abbr title="après Jésus‐Christ">ap.\302\240J.‑C.</abbr>' "$say_date_y"
-    elif [ "$say_date_y" -eq 0 ];then
-      printf '<span data-ssml-say-as="date" data-ssml-say-as-format="y">1</span>\302\240<abbr title="avant Jésus‐Christ">av.\302\240J.‑C.</abbr>'
+    elif [ "$say_date_y" -le 0 ];then
+      printf '<span data-ssml-say-as="date" data-ssml-say-as-format="y">%s</span>\302\240<abbr title="avant Jésus‐Christ">av.\302\240J.‑C.</abbr>' "$((1+$((-say_date_y))))"
     else
       printf '<span data-ssml-say-as="date" data-ssml-say-as-format="y">%s</span>' "$say_date_y"
     fi
   elif [ "$lang_l" = es ];then
     printf '<span data-ssml-say-as="date" data-ssml-say-as-format="d">%s</span>\302\240de\302\240' "$say_date_d"
     printf '%s de ' "$say_date_m_html"
-    if   [ "${#say_date_y}" -lt 4 ] &&
-         [ "$say_date_y" -ne 0 ];then
+    if   [ "$say_date_y" -lt 1000 ] &&
+         [ "$say_date_y" -gt 0 ];then
       printf '<span data-ssml-say-as="date" data-ssml-say-as-format="y">%s</span>\302\240<abbr title="después de Cristo">d.\302\240C.</abbr>' "$say_date_y"
-    elif [ "$say_date_y" -eq 0 ];then
-      printf '<span data-ssml-say-as="date" data-ssml-say-as-format="y">1</span>\302\240<abbr title="antes de Cristo">a.\302\240C.</abbr>'
+    elif [ "$say_date_y" -le 0 ];then
+      printf '<span data-ssml-say-as="date" data-ssml-say-as-format="y">%s</span>\302\240<abbr title="antes de Cristo">a.\302\240C.</abbr>' "$((1+$((-say_date_y))))"
     else
       printf '<span data-ssml-say-as="date" data-ssml-say-as-format="y">%s</span>' "$say_date_y"
     fi
