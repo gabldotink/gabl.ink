3aaa7d63b6eb78d762f1d5c46d6dc7203f41acee
Gabriel “gabldotink” <gabl@gabl.ink>
2026-05-23 13:54:09 -0600

    make region and script IDs canonically lowercase

--- cms/scripts/lib/parse_lang.sh
+++ cms/scripts/lib/parse_lang.sh
@@ -7,7 +7,7 @@ parse_lang(){
 
   parse_lang_loop=1
 
-  while [ "$parse_lang_loop" -le 2 ];do
+  while [ "$parse_lang_loop" -le 3 ];do
     eval 'parse_lang_'"$parse_lang_loop"'="$(printf "%s" "$lang"|cut -d- "-f$parse_lang_loop")"'
     parse_lang_loop="$((parse_lang_loop+1))"
   done
@@ -22,18 +22,30 @@ parse_lang(){
   if printf '%s\n' "$parse_lang_2"|grep '-qe^[A-Z]\{2\}$';then
     # shellcheck disable=2034
     lang_r="$parse_lang_2"
+    # shellcheck disable=2034
+    lang_r_l="$(jq -nr --arg r "$lang_r" '$r|ascii_downcase')"
   else
     err e 'Region subtag is not valid'
   fi
 
-  lang_s="$(jq -r --arg l "$lang_l" '.[$l].implicit.script' "$dict/language.json")"
+  if printf '%s\n' "$parse_lang_3"|grep '-qe^[A-Z][a-z]\{3\}$';then
+    # shellcheck disable=2034
+    lang_s="$parse_lang_3"
+  elif [ "$(jq -r --arg l "$lang_l" '.[$l].implicit.script' "$dict/language.json")" != null ];then
+    # shellcheck disable=2034
+    lang_s="$(jq -r --arg l "$lang_l" '.[$l].implicit.script' "$dict/language.json")"
+  else
+    err e 'Script subtag is not valid'
+  fi
+  # shellcheck disable=2034
+  lang_s_l="$(jq -nr --arg s "$lang_s" '$s|ascii_downcase')"
 
   # shellcheck disable=2034
-  lang_d="$(jq -r --arg l "$lang_s" '.[$l].dir' "$dict/script.json")"
+  lang_d="$(jq -r --arg s "$lang_s" '.[$s].dir' "$dict/script.json")"
 
   set_var_l10n lang_l_name "\"$lang_l\".name" "$dict/language.json"
-  set_var_l10n lang_r_name "\"$lang_r\".name" "$dict/region.json"
-  set_var_l10n lang_s_name "\"$lang_s\".name" "$dict/script.json"
+  set_var_l10n lang_r_name "\"$lang_r_l\".name" "$dict/region.json"
+  set_var_l10n lang_s_name "\"$lang_s_l\".name" "$dict/script.json"
 
-  lang_r_flag="$(printf '%s' "$lang_r"|jq -Rr 'explode|map(.-65+127462)|implode')"
+  lang_r_flag="$(jq -nr --arg r "$lang_r" '$r|explode|map(.-65+127462)|implode')"
 }
