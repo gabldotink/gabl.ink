aed48ffcafba7cd2c26f07c8cd615dffa5231e0f
Gabriel “gabldotink” <gabl@gabl.ink>
2026-07-05 00:07:10 -0600

    add comment to `fix.sh`

--- cms/scripts/fix.sh
+++ cms/scripts/fix.sh
@@ -4,5 +4,6 @@
 scripts="$(dirname "$0")"
 
 # This fixes problems that my computer introduces for some inexplicable reason
+# Note that `sed -i` is not POSIX
 sed -i -- "s/$(printf 'for\240. . . less')/for . . . less/g" "$scripts/../../i/jrco_beta/1/en-us/index.html"
 sed -i -- "s/$(printf 'moi \201\220même')/moi‐même/g" "$scripts/../../i/jrco_beta/1/fr-fr/index.html"
