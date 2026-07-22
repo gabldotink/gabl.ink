824962ab9b8bdcd7a7b2b589b98ca94ac390ec84
Gabriel “gabldotink” <gabl@gabl.ink>
2026-07-22 12:28:42 -0600

    put dictionaries in `data`

--- cms/scripts/build_beta.py
+++ cms/scripts/build_beta.py
@@ -16,12 +16,17 @@ scripts=Path(os.path.dirname(script))
 cms=Path(scripts)/".."
 lib=Path(scripts)/"lib"
 dicts=Path(cms)/"dictionaries"
-index=Path(cms)/"../i"
+index=Path(cms)/".."/"i"
 encyclopedia=Path(index)/"encyclopedia"
 
 data={}
 
 item_files=subprocess.run(["find",Path(index),"-type","f","-name","data.json"],capture_output=True,text=True).stdout.splitlines()
+
+for dict in ["copyright_license","disclaimer","language","month","region","script","share_link","string","validate_link"]:
+    data[f"dictionaries/{dict}"]={}
+    with open(Path(dicts)/f"{dict}.json","r",encoding="utf-8") as f:
+        data[f"dictionaries/{dict}"]["dictionary"]=json.load(f)
 
 for i in item_files:
     with open(i,"r",encoding="utf-8") as f:
@@ -29,9 +34,7 @@ for i in item_files:
         i_id=obj.get("id")
         data[i_id]=obj
 
-for dict in ["copyright_license","disclaimer","language","month","region","script","share_link","string","validate_link"]:
-    with open(Path(dicts)/f"{dict}.json","r",encoding="utf-8") as f:
-        globals()[f"dict_{dict}"]=json.load(f)
+print(data["dictionaries/copyright_license"])
 
 #def get_var_l10n(key,format,dict):
 #    try:
@@ -77,10 +80,10 @@ for i in item_files:
         canonical=f"https://gabl.ink/i/{str(data[i_id]["id"])}/{str(lang).lower()}/"
 
         print("<!DOCTYPE html>")
-        print(f"<!-- SPDX-License-Identifier: {dict_copyright_license[data[i_id]["copyright"]["license"][0]]["spdx"]} -->")
+        print(f"<!-- SPDX-License-Identifier: {data["dictionaries/copyright_license"]["dictionary"][data[i_id]["copyright"]["license"][0]]["spdx"]} -->")
 
         # TODO: Skipping the dir attribute for now, but it should be implemented later
-        print(f"<html lang={str(lang)}>",end="")
+        print(f"<html lang={lang}>",end="")
 
         print('<meta charset=utf-8><meta name=viewport content="width=device-width,initial-scale=1">',end="")
 
