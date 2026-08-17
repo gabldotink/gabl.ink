ed59272841c5b027139a0800822aa2049915d766
Gabriel “gabldotink” <gabl@gabl.ink>
2026-08-17 09:40:02 -0600

    force `-O2`

--- cms/scripts/cython.sh
+++ cms/scripts/cython.sh
@@ -4,6 +4,6 @@ set -x
 d="$(dirname -- "$0")"
 cython --embed "-3o$d/build.c" -- "$d/build.py"
 # shellcheck disable=2046
-gcc -O2 $(python3-config --cflags --embed --ldflags) "-o$d/build" "$d/build.c"
+gcc $(python3-config --cflags --embed --ldflags) -O2 "-o$d/build" "$d/build.c"
 rm -f -- "$d/build.c"
 strip -- "$d/build"
