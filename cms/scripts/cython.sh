1ee8bc44641213181deef28ba34a88338f8bff50
Gabriel “gabldotink” <gabl@gabl.ink>
2026-08-17 00:19:57 -0600

    use(?) `-O2`
    `python3-config --cflags` seems to be adding `-O3` anyway
    
    I only used `-Ofast` as an experiment in the first place

--- cms/scripts/cython.sh
+++ cms/scripts/cython.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # SPDX-License-Identifier: CC0-1.0
+set -x
 d="$(dirname -- "$0")"
 cython --embed "-3o$d/build.c" -- "$d/build.py"
 # shellcheck disable=2046
-gcc -Ofast $(python3-config --cflags) $(python3-config --embed --ldflags) "-o$d/build" "$d/build.c"
+gcc -O2 $(python3-config --cflags) $(python3-config --embed --ldflags) "-o$d/build" "$d/build.c"
 rm -f -- "$d/build.c"
 strip -- "$d/build"
