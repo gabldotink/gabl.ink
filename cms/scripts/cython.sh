da8bfb7c30293c1b2c77da8a5de97c91ae5ead1a
Gabriel “gabldotink” <gabl@gabl.ink>
2026-08-17 13:07:30 -0600

    idk how GCC works

--- cms/scripts/cython.sh
+++ cms/scripts/cython.sh
@@ -4,6 +4,6 @@ set -x
 d="$(dirname -- "$0")"
 cython --embed "-3o$d/build.c" -- "$d/build.py"
 # shellcheck disable=2046
-gcc $(python3-config --cflags --embed --ldflags) -O2 "-o$d/build" "$d/build.c"
+gcc "$d/build.c" $(python3-config --cflags --embed --ldflags) -O2 "-o$d/build"
 rm -f -- "$d/build.c"
 strip -- "$d/build"
