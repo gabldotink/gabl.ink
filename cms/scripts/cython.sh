be03881d61c0fa58a107ac16f9f895241b320dc3
Gabriel “gabldotink” <gabl@gabl.ink>
2026-08-23 09:50:48 -0600

    whatever

--- cms/scripts/cython.sh
+++ cms/scripts/cython.sh
@@ -2,8 +2,8 @@
 # SPDX-License-Identifier: CC0-1.0
 set -x
 d="$(dirname -- "$0")"
-cython --embed "-3o$d/build.c" -- "$d/build.py"
+cython -3 --embed --output-file="$d/build.c" -- "$d/build.py"
 # shellcheck disable=2046
-gcc "$d/build.c" $(python3-config --cflags --embed --ldflags) -O2 "-o$d/build"
+gcc "$d/build.c" $(python3-config --cflags --embed --ldflags) -O2 -o "$d/build"
 rm -f -- "$d/build.c"
 strip -- "$d/build"
