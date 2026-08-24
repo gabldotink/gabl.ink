516fd76e728d7b345e68242f76cf37c8ee0cc676
Gabriel “gabldotink” <gabl@gabl.ink>
2026-08-23 19:51:20 -0600

    remove Cython for now

--- gabel/cython.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# SPDX-License-Identifier: CC0-1.0
-d="$(dirname -- "$0")"
-cython -3 --embed --output-file="$d/gabel/build.c" -- "$d/gabel/build.py"
-# shellcheck disable=2046
-gcc "$d/gabel/build.c" $(python3-config --cflags --embed --ldflags) -O2 -o "$d/gabel/build"
-rm -f -- "$d/gabel/build.c"
-strip -- "$d/gabel/build"
---
+++ gabel/gabel/get_var_l10n.py
@@ -0,0 +1,36 @@
+# SPDX-License-Identifier: CC0-1.0
+
+from langcodes import Language
+
+def get_var_l10n(index,key:str|int,format:str,l10n_lang:Language)->str:
+    # e.g. get_var_l10n(data["jrco_beta/1"]["location"],"series","text",lang)
+
+    for o in str(l10n_lang),str(l10n_lang.language),"mul","zxx","e":
+        if o=="e":
+            return None
+
+        if format=="id":
+            if "id" in index.get(key,{}).get(o,{}):
+                return index[key][o]["id"]
+            elif "equal" in index.get(key,{}).get(o,{}):
+                return get_var_l10n(index,key,format,Language.get(index[key][o]["equal"]))
+
+        if format=="print":
+            if "print" in index.get(key,{}).get(o,{}):
+                return index[key][o]["print"]
+            elif "equal" in index.get(key,{}).get(o,{}):
+                return get_var_l10n(index,key,format,Language.get(index[key][o]["equal"]))
+
+        if format=="text":
+            if "text" in index.get(key,{}).get(o,{}):
+                return index[key][o]["text"]
+            elif "equal" in index.get(key,{}).get(o,{}):
+                return get_var_l10n(index,key,format,Language.get(index[key][o]["equal"]))
+
+        if format=="html":
+            if "html" in index.get(key,{}).get(o,{}):
+                return index[key][o]["html"]
+            elif "text" in index.get(key,{}).get(o,{}):
+                return index[key][o]["text"]
+            elif "equal" in index.get(key,{}).get(o,{}):
+                return get_var_l10n(index,key,format,Language.get(index[key][o]["equal"]))
